bd2e4631a60f60300dd69c621d48e1afab7ee369
copper-light <gmanhdh@gmail.com>
2022-10-11 05:03:09 +0000

    update tensorrt options

--- model/pytorch/unet/convert_tensorrt.py
+++ model/pytorch/unet/convert_tensorrt.py
@@ -7,7 +7,7 @@ def run():
                     "--saveEngine=check_points/unet/model.engine",
                     "--verbose",
                     "--workspace=512",
-                    "--fp32"])
+                    "--noTF32"])
 
 if __name__ == '__main__':
     run()
