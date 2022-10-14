b70fd92e1acb5fa80ade27f59dd8959b06712086
copper-light <gmanhdh@gmail.com>
2022-10-14 06:18:51 +0000

    update load image

--- model/pytorch/unet/dataset.py
+++ model/pytorch/unet/dataset.py
@@ -11,14 +11,20 @@ from torchvision.transforms import transforms
 
 from torch.utils.data import Dataset
 
-def load_image(path):
-    img = cv2.imread(path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = cv2.resize(img, (256, 256))
-    img = img.astype(np.float32) / 255.
-    img = np.transpose(img, (2, 0, 1))
-    return img
+from PIL import Image
+from io import BytesIO
 
+def load_image(path, use_opencv= True):
+    if use_opencv:
+        img = cv2.imread(path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, (256, 256))
+        img = np.transpose(img, (2, 0, 1))
+        img = img.astype(np.float32) / 255.
+    else:
+        img = Image.open(BytesIO(buf))
+        
+    return img
 
 def load_data(root_path, name='images', trim=None):
     paths = glob(os.path.join(root_path, name, "*.png"))
