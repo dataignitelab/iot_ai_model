14c4f0c88501daced2a9f43bb0d1167794f2af4e
copper-light <gmanhdh@gmail.com>
2022-11-21 01:41:48 +0000

    REMOVE: temp files

--- model/pytorch/inception/.ipynb_checkpoints/model-checkpoint.py
+++
@@ -1,6 +0,0 @@
-import timm
-
-def inceptionv4(num_classes = 1):
-    model = timm.create_model('inception_v4', pretrained=False, num_classes = 1)
-
-    return model
--- model/tensorflow/resnet/.ipynb_checkpoints/onnx_to_rt-checkpoint.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-trtexec --onnx=check_points/resnet50/model.onnx --saveEngine=check_points/resnet50/model.engine --verbose --best
